2bfb0509cf3daec6a105a0414a9e231013ab0d04
Thiago-Martins-1 <82213624+Thiago-Martins-1@users.noreply.github.com>
2021-11-18 10:56:12 -0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,12 +1,25 @@
 #include "libs/analog.h"
 #include "libs/LCD.h"
 
+
 int main(void) {
+    
+    int TT;
+    int TD;
+    LCD lcd(0,1,2,3,4,5); 
 
     while(1) {
 
-        // TODO
+        TT = analogRead(4);  
+        TD = analogRead(3);
+        float TN = (float) TT * 5 / 1023;
+        float TN2 = (float) TD * 5 / 1023;
+        float TMP = (TN - TN2) * 100;       // El 100 multiplicaba solo a TN2
+        lcd.clear();
+        lcd.print(TMP);
+
     }
 
     return 0;
+
 }
